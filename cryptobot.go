94a0b792c08de6f329e86706423f14c9c619dcc4
Vivek Kartha <vivek.kartha@mobme.in>
2018-02-13 16:03:51 +0530

    Added binance wrapper | all open orders displayed

--- cryptobot.go
+++ cryptobot.go
@@ -4,13 +4,24 @@ import (
 	"time"
 	"log"
 
-
 	conf "github.com/vivekkartha/cryptobot/config"
 	tb "gopkg.in/tucnak/telebot.v2"
+
+	"github.com/vivekkartha/go-binance/binance"
+	"fmt"
 )
 
 func main() {
 
+	client := binance.New(conf.BinanceApiKey, conf.BinanceSecret)
+	res, err := client.GetAllOpenOrders()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _,v := range res{
+		fmt.Println(v.Symbol,"Qty:",v.OrigQty,"| Price:",v.Price, "| Type:",v.Type,v.Side)
+	}
 	b, err := tb.NewBot(tb.Settings{
 		Token:  conf.TelegramToken,
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
@@ -21,10 +32,17 @@ func main() {
 		return
 	}
 
+	var recipient tb.Recipient
+
 	log.Println("Bot started")
 	log.Println("Listening for messages...")
+
 	b.Handle("/start", func(m *tb.Message) {
-		b.Send(m.Sender, "Hey there!")
+		recipient = m.Sender
+		fmt.Println("Command")
+		b.Send(m.Sender,"Hey!")
+		log.Println(m.Text)
 	})
+
 	b.Start()
 }
